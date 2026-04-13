cd5d9c12059705d0198c2b88e5ac4e7a5a9f17fa
Anjalipradeep06 <anjalipradeep1214@gmail.com>
2026-04-13 09:39:20 +0530

    title updated

--- src/pages/ContactUs/Contact.jsx
+++ src/pages/ContactUs/Contact.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Contact.css";
 
-function Contact() {
+function contact() {
   return (
     <div className="contact-box">
       <h1>📞 Contact Us</h1>
@@ -22,4 +22,4 @@ function Contact() {
   );
 }
 
-export default Contact;
+export default contact;
